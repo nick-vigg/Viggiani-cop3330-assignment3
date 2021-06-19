b870ae5ac0507ad3a843daf66db349e78c7ab8fe
nick-vigg <nviggiani@knights.ucf.edu>
2021-06-19 14:20:01 -0400

    Second Commit

--- src/main/java/ex42/ParsingADataFile.java
+++ src/main/java/ex42/ParsingADataFile.java
@@ -1,3 +1,35 @@
+/*
+UCF COP3330 Summer 2021 Assignment 3 Solution
+Copyright 2021 Nicholas Viggiani
+Exercise 42 - Parsing a Data File
+Sometimes data comes in as a structured format that you have to break down and turn into records so you
+can process them. CSV, or comma-separated values, is a common standard for doing this.
+
+Construct a program that reads in the following data file (you will need to create this data file yourself;
+name it `exercise42_input.txt`):
+
+Ling,Mai,55900
+Johnson,Jim,56500
+Jones,Aaron,46000
+Jones,Chris,34500
+Swift,Geoffrey,14200
+Xiong,Fong,65000
+Zarnecki,Sabrina,51500
+Process the records and display the results formatted as a table, evenly spaced, as shown in the example output.
+
+Example Output
+Last      First     Salary
+--------------------------
+Ling      Mai       55900
+Johnson   Jim       56500
+Jones     Aaron     46000
+Jones     Chris     34500
+Swift     Geoffrey  14200
+Xiong     Fong      65000
+Zarnecki  Sabrina   51500
+Constraints
+Write your own code to parse the data. Don't use a CSV parser
+ */
 package ex42;
 
 import java.util.*;
