55127f798bac03c2366bd419f025e1b84177af35
nick-vigg <nviggiani@knights.ucf.edu>
2021-06-18 20:35:58 -0400

    Second Commit

--- src/main/java/ex42/ParsingADataFile.java
+++ src/main/java/ex42/ParsingADataFile.java
@@ -7,16 +7,14 @@ import java.util.Collections;
 
 public class ParsingADataFile {
 
-        public static void main(String[] args) throws IOException {
-            String filePath = "C:\\Users\\19172\\IdeaProjects\\BookExample1\\cop3330-as3\\src\\main\\java\\ex42\\exercise42_input";
-            ArrayList<Map<String, String>> nameList = Input.read(filePath); //takes in list of maps containing first, last and salary of employees.
-            //String output = file.print(nameList);
+    public static void main(String[] args) throws IOException {
+        String filePath = "C:\\Users\\19172\\IdeaProjects\\BookExample1\\cop3330-as3\\src\\main\\java\\ex42\\exercise42_input";
+        ArrayList<Map<String, String>> nameList = Input.read(filePath);
 
-            System.out.println();
-            //Collections.sort(nameList, new SortSalary()); //sorts based on salary
+        System.out.println();
 
-            String output = Input.print(nameList);
-            System.out.println(output);
-        }
+        String output = Input.print(nameList);
+        System.out.println(output);
     }
+}
 
